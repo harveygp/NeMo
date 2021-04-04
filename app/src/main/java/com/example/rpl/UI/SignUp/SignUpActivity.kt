2597a052855cccdd3bf01f6fb7bb9a7aeeae1b9d
naravey <harvey.narukami@gmail.com>
2021-04-04 20:34:32 +0700

    Sprint 3

--- app/src/main/java/com/example/rpl/UI/SignUp/SignUpActivity.kt
+++ app/src/main/java/com/example/rpl/UI/SignUp/SignUpActivity.kt
@@ -1,8 +1,12 @@
 package com.example.rpl.UI.SignUp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rpl.R
+import com.example.rpl.UI.Login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 class SignUpActivity : AppCompatActivity(){
 
@@ -10,5 +14,8 @@ class SignUpActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_signup)
 
+        register.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        })
     }
 }
